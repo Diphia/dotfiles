a58f6b693a730b2c5a7fdb5eb88c5a9b97574e11
Diphia <zqygodness@gmail.com>
2019-09-28 20:55:20 +0800

    zshrc added

---
+++ dotfiles_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -si ~/dotfiles/.vimrc ~/.vimrc
+ln -si ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -si ~/dotfiles/vifmrc ~/.config/vifm/vifmrc
+ln -si ~/dotfiles/ssh_config ~/.ssh/config
+ln -si ~/dotfiles/.zshrc ~/.zshrc
