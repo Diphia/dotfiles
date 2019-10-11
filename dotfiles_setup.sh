e7d00f23480fc73020181e893009c70fd10722f0
Diphia <zqygodness@gmail.com>
2019-10-11 15:23:07 +0800

    vim script added

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -3,9 +3,21 @@
 # This script is used to create the soft links which point to the authentic config file locations
 
 ln -si ~/dotfiles/.vimrc ~/.vimrc
+
 ln -si ~/dotfiles/.tmux.conf ~/.tmux.conf
+
 ln -si ~/dotfiles/vifmrc ~/.config/vifm/vifmrc
+
 ln -si ~/dotfiles/ssh_config ~/.ssh/config
+
 ln -si ~/dotfiles/.zshrc ~/.zshrc
+
 ln -si ~/dotfiles/i3_config ~/.config/i3/config
+
 ln -si ~/dotfiles/i3status_config ~/.config/i3status/config
+
+if [ ! -d "~/.vim/autoload/" ]
+then
+	mkdir ~/.vim/autoload/	
+fi
+ln -si ~/dotfiles/plug.vim ~/.vim/autoload/plug.vim
