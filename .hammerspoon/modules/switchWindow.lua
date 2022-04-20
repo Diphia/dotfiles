08059037a1f5856778c139f432ef808d62046581
qiyan <qiyazhan@cisco.com>
2022-04-20 17:51:42 +0800

    update

--- .hammerspoon/modules/switchWindow.lua
+++ .hammerspoon/modules/switchWindow.lua
@@ -1,31 +1,47 @@
 hs.hotkey.bind({"control"}, "1", function()
     hs.window'GNU Emacs\n':focus()
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control"}, "2", function()
     hs.application.launchOrFocus("iTerm")
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control"}, "3", function()
     hs.application.launchOrFocus("Google Chrome")
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control", "command"}, "3", function()
     hs.application.launchOrFocus("Safari")
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control"}, "4", function()
     hs.window'Org Mode\n':focus()
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"command", "option"}, ",", function()
     hs.application.launchOrFocus("System Preferences")
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control"}, "q", function()
     hs.application.launchOrFocus("Webex")
+    moveCursorToFocusedWindow()
 end)
 
 hs.hotkey.bind({"control", "command"}, "q", function()
     hs.application.launchOrFocus("Microsoft Outlook")
+    moveCursorToFocusedWindow()
 end)
+
+function moveCursorToFocusedWindow()
+    local current = hs.window.focusedWindow()
+    local screen = current:screen()
+    local rect = screen:fullFrame()
+    local center = hs.geometry.rectMidPoint(rect)
+    hs.mouse.setAbsolutePosition(center)
+end
