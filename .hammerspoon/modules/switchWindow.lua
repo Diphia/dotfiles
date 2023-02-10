362c26bc4c4ae36d083a983f4e202e1814947b2c
qiyan <qiyazhan@cisco.com>
2023-02-10 17:14:17 +0800

    store emacs windows to make switch faster

--- .hammerspoon/modules/switchWindow.lua
+++ .hammerspoon/modules/switchWindow.lua
@@ -1,5 +1,14 @@
+local emacs
+local org_mode
+emacs = hs.window'GNU Emacs\n'
+org_mode = hs.window'Org Mode\n'
+
+
 hs.hotkey.bind({"option"}, "1", function()
-    hs.window'GNU Emacs\n':focus()
+    if not emacs then
+        emacs = hs.window'GNU Emacs\n'
+    end
+    emacs:focus()
     moveCursorToFocusedWindow()
 end)
 
@@ -19,7 +28,10 @@ hs.hotkey.bind({"option"}, "s", function()
 end)
 
 hs.hotkey.bind({"option"}, "4", function()
-    hs.window'Org Mode\n':focus()
+    if not org_mode then
+        org_mode = hs.window'Org Mode\n'
+    end
+    org_mode:focus()
     moveCursorToFocusedWindow()
 end)
 
@@ -47,6 +59,34 @@ hs.hotkey.bind({"option"}, "w", function()
     hs.application.launchOrFocus("WeChat")
     moveCursorToFocusedWindow()
 end)
+
+hs.hotkey.bind({"option"}, "e", function()
+    hs.application.launchOrFocus("Telegram")
+    moveCursorToFocusedWindow()
+end)
+
+hs.hotkey.bind({"option"}, "m", function()
+    hs.application.launchOrFocus("Cisco Webex Meetings")
+    moveCursorToFocusedWindow()
+end)
+
+hs.hotkey.bind({"option"}, "t", function()
+    hs.application.launchOrFocus("Terminal")
+    moveCursorToFocusedWindow()
+end)
+
+-- bind hotkey
+hs.hotkey.bind({'option'}, 'a', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+  moveCursorToFocusedWindow()
+end)
+
 
 function moveCursorToFocusedWindow()
     local current = hs.window.focusedWindow()
