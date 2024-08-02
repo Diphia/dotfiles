35a21c15028c05b487f35275e7847bcf41ca9a86
qiyan zhang <qiyazhan@cisco.com>
2024-08-02 13:32:36 +0800

    delete doom config

--- .doom.d/emacs-yaml-tools/eyt-convert-to-ut.el
+++
@@ -1,9 +0,0 @@
-;;; ../dotfiles/.doom.d/emacs-yaml-tools/eyt-convert-to-ut.el -*- lexical-binding: t; -*-
-
-(defun eyt-convert-to-ut ()
-  (interactive)
-  (let* ((line (fetch-current-line))
-         (value (extract-yaml-value line))
-         (path (form-path-with-dot (fetch-yaml-path))))
-    (kill-new (concat "\s\s\s\s\s\s- equal:\n" "\s\s\s\s\s\s\s\s\s\spath: " path "\n\s\s\s\s\s\s\s\s\s\svalue: " value))
-    (message (concat "path:" path "  value:" value))))
